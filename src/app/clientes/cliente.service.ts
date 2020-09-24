86722626051540abe680409c8f2772ab852921e8
Kevin Martinez <kkevinmartinez7@gmail.com>
2020-09-24 16:51:22 -0600

    Using Observer

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Cliente } from './cliente';
 import { CLIENTES } from './clientes.json';
+import {Observable, of} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,5 +10,7 @@ export class ClienteService {
 
   constructor() { }
 
-  getClientes(): Cliente[] {return CLIENTES; }
+  getClientes(): Observable< Cliente[]> {
+    return of(CLIENTES);
+   }
 }
