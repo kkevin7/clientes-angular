16f7431fb69161273270f798c3addb6f5563bc06
Kevin Martinez <kkevinmartinez7@gmail.com>
2020-09-25 00:22:39 -0600

    show data from get request

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { NavbarComponent } from './layouts/navbar/navbar.component';
 import { FooterComponent } from './layouts/footer/footer.component';
 import { DirectivaComponent } from './directiva/directiva.component';
 import { ClientesComponent } from './clientes/clientes.component';
+import { HttpClientModule } from '@angular/common/http';
 
 const routes: Routes = [
   {path: '', redirectTo: '/clientes', pathMatch: 'full'},
@@ -26,8 +27,10 @@ const routes: Routes = [
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+
   ],
   providers: [ClienteService],
   bootstrap: [AppComponent]
--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -2,15 +2,22 @@ import { Injectable } from '@angular/core';
 import { Cliente } from './cliente';
 import { CLIENTES } from './clientes.json';
 import {Observable, of} from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClienteService {
+  private urlEndPoint: string = 'http://localhost:8080/api/clientes';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
-  getClientes(): Observable< Cliente[]> {
-    return of(CLIENTES);
-   }
+  getClientes(): Observable<Cliente[]> {
+    //return of(CLIENTES);
+    return this.http.get(this.urlEndPoint).pipe(
+      map(response => response as Cliente[])
+    );
+  }
+
 }
