d44acb296e3aab2b40aba32de3cdb58700f78773
Kevin Martinez <kkevinmartinez7@gmail.com>
2020-09-25 02:43:47 -0600

    create and sweetalert2 message

--- src/app/clientes/form.component.ts
+++ src/app/clientes/form.component.ts
@@ -1,5 +1,8 @@
+import { ClienteService } from './cliente.service';
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
+import {Router} from '@angular/router';
+import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-form',
@@ -8,17 +11,27 @@ import { Cliente } from './cliente';
 })
 export class FormComponent implements OnInit {
 
-  private cliente: Cliente = new Cliente();
-  private titulo: string = "Crear Cliente";
+  cliente: Cliente = new Cliente();
+  titulo: string = "Crear Cliente";
 
-  constructor() { }
+  constructor(private clienteService: ClienteService, private router: Router) { }
+
 
   ngOnInit(): void {
   }
 
   public create(): void {
-    console.log("Clicked!");
-    console.log(this.cliente);
+    this.clienteService.create(this.cliente).subscribe(
+      cliente => {
+        this.router.navigate(['/clientes']);
+        Swal.fire({
+          title: 'Nuevo Cliente',
+          text: `Cliente ${cliente.nombre} creado con éxito!`,
+          icon: 'success',
+          confirmButtonText: 'Ok'
+        })
+      }
+    );
   }
 
 }
